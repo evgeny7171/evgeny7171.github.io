a6ec3619445e3017bcad976d947afa37391a153f
evgeny7171 <evgeny1231@bigmir.net>
2022-06-21 00:28:34 +0300

    Add files via upload

---
+++ Blink.js
@@ -0,0 +1,20 @@
+<script>
+
+function blink()
+{
+	var div = document.getElementById('video');
+	if (div.style.visibility=="hidden")
+	{
+		div.style.visibility="visible";
+	}
+	else
+	{
+		div.style.visibility="visible";
+	}
+}
+
+// blink every 500 ms
+
+window.setInterval(blink, 500);
+
+</script>
